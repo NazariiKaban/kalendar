9b8d343c2caf2b307091ac5c032e7bef5dcc1326
Nazarii Fedak <nazar1fedak@gmail.com>
2026-03-23 14:49:48 +0100

    calendar pred end version

--- script.js
+++ script.js
@@ -38,3 +38,65 @@ function showTask() {
 }
 
 showTask()
+
+
+const monthYearEl = document.getElementById('month-year');
+const daysContainer = document.getElementById('calendar-days');
+const dayNamesContainer = document.querySelector('calendar_day-names');
+const prevBtn = document.getElementById('prev-btn');
+const nextBtn = document.getElementById('next-btn');
+
+//Массивы месяцев и дней недели
+
+const monthNames = [
+  'January', 'February', 'March',
+   'April', 'May', 'June',
+   'July', 'August', 'September',
+    'October', 'November', 'December'
+];
+
+const dayNames = [
+  'Monday', 'Tuesday', 'Wednesday', 'Thursday',
+   'Friday', 'Saturday', 'Sunday'
+];
+
+let currentDate = new Date();
+
+const renderDayNames = () => {
+  dayNamesContainer.innerHTML = dayNames.map(day => 
+    `<span>${day}</span>`).join(``);
+}
+
+const renderCalendar = () => {
+  const year = currentDate.getFullYear();
+  const mont = currentDate.getMonth();
+
+  monthYearEl.textContent = `${monthNames[month]} ${year}`;
+
+  const firstDay = (new Date(year,month).getDay() + 6) % 7;
+  const daysInMonth = 32 - new Date(year,month, 32).getDate();
+
+  daysContainer.innerHTML = '';
+
+  for (let i = 0; i  < firstDay; i++) {
+      daysContainer.innerHTML += '<span class="calendar_days-hidden"></span>'s;
+  } 
+
+  for (let day = 1; day <= daysInMonth; day++) {
+    const isToday = day === new Date().getDate() &&
+                        month === new Date().getMonth() &&
+                        year === new Date().getFullYear;
+
+  daysContainer.innerHTML += `<span class="${isToday ? 'today' : ''}">${day}</span>`
+  }
+}
+const changeMonth = (dalta) => {
+  currentDate.setDateMomtj(currentDate.getMonth() + delta);
+  renderCalendar();
+}
+
+prevBtn.addEventListener('click',() => changeMonth(-1));
+nextBtn.addEventListener('click',() => changeMonth(1));
+
+renderDayNames();
+renderCalendar();
